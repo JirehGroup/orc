96af7a69944b4490015ad2951e35e1fa28b324bf
JirehGroup <accounts@jirehgrp.com>
2025-03-26 02:23:05 +0300

    frontend: homepage update with a header

--- src/components/common/Hero.tsx
+++ src/components/common/Hero.tsx
@@ -7,6 +7,9 @@ import { LayoutGrid } from "@/components/ui/aceternity/layout-grid";
 export function Hero() {
   return (
     <div className="h-screen py-20 w-full">
+      <h1 className="text-4xl text-center font-bold text-dark dark:text-white mb-8">
+        Welcome to Inter-Religious Council of Oromia
+      </h1>
       <LayoutGrid cards={cards} />
     </div>
   );
