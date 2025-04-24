cfd233b0e178a2000d346fb8f339f4895fc475c6
JirehGroup <accounts@jirehgrp.com>
2025-04-24 21:15:40 +0300

    flag update

--- src/components/common/flag.tsx
+++ src/components/common/flag.tsx
@@ -1,28 +1,10 @@
-// src/components/common/Flag.tsx
-
-import React from 'react';
-
-const Flag = () => {
-  return (
-    <div className="fixed right-0 top-0 h-full w-screen sm:w-[40px] z-50 flex flex-row">
-      <div className="w-1/3 h-full bg-red-600" />
-      <div className="w-1/3 h-full bg-white" />
-      <div className="w-1/3 h-full bg-black" />
-    </div>
-  );
-};
-
-export default Flag;
-
-
-
 // // src/components/common/Flag.tsx
 
 // import React from 'react';
 
 // const Flag = () => {
 //   return (
-//     <div className="fixed left-0 top-0 h-screen w-[40px] z-50 flex flex-row">
+//     <div className="fixed right-0 top-0 h-full w-screen sm:w-[40px] z-50 flex flex-row">
 //       <div className="w-1/3 h-full bg-red-600" />
 //       <div className="w-1/3 h-full bg-white" />
 //       <div className="w-1/3 h-full bg-black" />
@@ -31,3 +13,21 @@ export default Flag;
 // };
 
 // export default Flag;
+
+
+
+// src/components/common/Flag.tsx
+
+import React from 'react';
+
+const Flag = () => {
+  return (
+    <div className="fixed right-0 top-0 h-screen w-[40px] z-50 flex flex-row">
+      <div className="w-1/3 h-full bg-red-600" />
+      <div className="w-1/3 h-full bg-white" />
+      <div className="w-1/3 h-full bg-black" />
+    </div>
+  );
+};
+
+export default Flag;
